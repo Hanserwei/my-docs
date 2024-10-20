f3340fafce87cb88835ce0f1a17fb4319439653b
Hanserwei <2628273921@qq.com>
2024-10-20 19:33:10 +0800

    first commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,53 @@
+import { defineConfig } from 'vitepress'
+import { set_sidebar } from './utils/auto_sidebar.mjs'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Hanser的笔记网站",
+  description: "一个基于vitepress的站点",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '家', link: '/' },
+    ],
+
+    footer:{
+      copyright: "Copyright@ 2024 Hanser Wei"
+    },
+    
+    sidebar: [
+      {
+        text: '千里之行',
+        items: [
+          { text: '', link: '/guide/index' }
+        ]
+      },
+      {
+        text: 'Python学习',
+        items: [
+          { text: '基础语法', link: '/guide/Python/基础语法部分/Python基础学习' },
+          { text: '进阶语法', link: '/guide/Python/进阶语法部分/Python进阶学习' },
+          { text: '第三方包', link: '/guide/Python/第三方包/index' },
+        ]
+      },
+      {
+        text: 'Java学习',
+        items: [
+          { text: '', link: '/guide/index' }
+        ]
+      },
+      {
+        text: 'PVE学习',
+        items: [
+          { text: 'PVE简介', link: '/guide/PVE/PVE简介' },
+          { text: 'PVE安装教程', link: '/guide/PVE/PVE安装教程' },
+          { text: 'PVE使用技巧', link: '/guide/PVE/PVE使用技巧' },
+        ]
+      },
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/Hanserwei' }
+    ]
+  }
+})
